8db25fcddb5f5860879c6b2022b4058c33301772
Dmitry Valkovets <dval@softserveinc.com>
2017-05-30 09:56:07 +0300

    first commit7

---
+++ HostMain/App_Start/WebApiConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+
+namespace CustomerTimesTask
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.EnableCors();
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new {id = RouteParameter.Optional});
+        }
+    }
+}
